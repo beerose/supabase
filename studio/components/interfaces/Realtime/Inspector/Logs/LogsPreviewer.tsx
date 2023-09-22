78032398dcfb48cabddacb0035936e823441cb74
Ivan Vasilov <vasilov.ivan@gmail.com>
2023-09-22 09:25:26 +0200

    Snapshot.

--- studio/components/interfaces/Realtime/Inspector/Logs/LogsPreviewer.tsx
+++ studio/components/interfaces/Realtime/Inspector/Logs/LogsPreviewer.tsx
@@ -27,10 +27,11 @@ export const RealtimeLogsPreviewer = ({
 }: RealtimeLogsPreviewerProps) => {
   const [realtimeConfig, setRealtimeConfig] = useState<UseRealtimeLogsPreviewParams>({
     enabled: false,
-    projectRef: '',
+    projectRef: 'pxidrgkatumlvfqaxcll',
     channelName: 'room_a',
     logLevel: 'info',
-    token: '',
+    token:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4aWRyZ2thdHVtbHZmcWF4Y2xsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njg5OTUzOTgsImV4cCI6MTk4NDU3MTM5OH0.d_yYtASLzAoIIGdXUBIgRAGLBnNow7JG2SoaNMQ8ySg',
     schema: 'public',
     table: '*',
     filter: undefined,
